c85f2b295d21146b4681349542e082c674ba50f4
arlai <arlai@moonkitt.com>
2024-05-23 09:24:37 +0200

    feat(vue3): adding the "with refresh" buttons

--- examples/vue3/src/app/App.vue
+++ examples/vue3/src/app/App.vue
@@ -10,6 +10,10 @@ import MyComponent from '../components/MyComponent.vue';
       <RouterLink to="/cosmoshub/">Switch to Cosmos Hub</RouterLink>
       |
       <RouterLink to="/osmosis/">Switch to Osmosis</RouterLink>
+      |
+      <a href="/cosmoshub/" class="">Switch to Cosmos Hub (with refresh)</a>
+      |
+      <a href="/osmosis/" class="">Switch to Osmosis (with refresh)</a>
     </nav>
   </header>
   <RouterView />
